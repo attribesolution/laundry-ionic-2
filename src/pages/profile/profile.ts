2125650b9df38770ced6fb99308991ce6cc15f57
Syed M. Taha <s.m.taha10@gmail.com>
2017-02-11 18:02:50 +0500

    Add Profile page and menu

---
+++ src/pages/profile/profile.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'profile',
+  templateUrl: 'profile.html'
+})
+export class ProfileComponent {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+}
