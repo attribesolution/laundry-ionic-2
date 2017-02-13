7f39d1011473cd76178b8c27a8ffa1651cdb9f57
Syed M. Taha <s.m.taha10@gmail.com>
2017-02-13 15:46:35 +0500

    Merge conflict solve

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 
-<<<<<<< HEAD
 import { NavController } from 'ionic-angular';
 
-=======
->>>>>>> b3071c9da443a2a0095a400d70909d91e80f3cc5
 @Component({
   selector: 'profile',
   templateUrl: 'profile.html'
 })
-<<<<<<< HEAD
+
 export class ProfileComponent {
 
   constructor(public navCtrl: NavController) {
@@ -17,9 +14,3 @@ export class ProfileComponent {
   }
 
 }
-=======
-
-export class ProfileComponent {
-
-}
->>>>>>> b3071c9da443a2a0095a400d70909d91e80f3cc5
