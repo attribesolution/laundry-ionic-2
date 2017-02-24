bf62fe9e01326cc06a75fe55f4aee8de428b6d99
Syed M. Taha <s.m.taha10@gmail.com>
2017-02-24 16:10:51 +0500

    Add notification screen

---
+++ src/pages/order-placed/order-placed.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component ({
+    selector: 'congratulation-component',
+    templateUrl: 'order-placed.html'
+})
+
+export class OrderPlaced{
+     constructor(){}
+}
