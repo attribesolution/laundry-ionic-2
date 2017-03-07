a54c46eec6c83dfdd4565f1d8f94099c61bc99e0
Syed M. Taha <s.m.taha10@gmail.com>
2017-03-07 16:48:08 +0500

    Working Date picker.

--- src/pages/drop-off-details/drop-off-details.ts
+++ src/pages/drop-off-details/drop-off-details.ts
@@ -17,7 +17,16 @@ export class DropOffDetails{
      minutes: number[] = Array.from(new Array(60),(val,index)=>index)
      dates = [];
      amPm: String[] = ['AM', 'PM'];
-     highlightedDiv: number;
+     highlightedDay: number;
+     highlightedHour: number;
+     highlightedMinute : number;
+     highlightedAmPm: number;
+     selectedDate = {
+        day: new Date(),
+        hour: 0,
+        minute: 0,
+        amPm: 'AM'
+     };
 
      dateArrayMaker(){
         for(let i = 0; i <= 9; i++)
@@ -34,17 +43,31 @@ export class DropOffDetails{
      getClassofDate(e){
 
      }
-     toggleHighlight(newValue: number){
-         if (this.highlightedDiv === newValue) {
-            this.highlightedDiv = 0;
-        }
-        else {
-            this.highlightedDiv = newValue;
-        }
-     }
-     startNextScreen()
-        {
+     toggleHighlight(Elementid: any, segment: string){
+        console.clear();
+        console.log(Elementid);
+        this.selectedDate[segment] = Elementid;
+        console.log(this.selectedDate);
+        
+        
+        segment === 'day' ? 
+            this.highlightedDay === Elementid ? this.highlightedDay = 0 : this.highlightedDay = Elementid :
+        segment === 'hour' ? 
+            this.highlightedHour === Elementid ? this.highlightedHour = 0 : this.highlightedHour = Elementid :
+        segment === 'minute' ? 
+            this.highlightedMinute === Elementid ? this.highlightedMinute = 0 : this.highlightedMinute = Elementid :
+        segment === 'amPm' ? 
+            this.highlightedAmPm === Elementid ? this.highlightedAmPm = 0 : this.highlightedAmPm = Elementid : '';
+    }
+     startNextScreen(){
             this.navCtrl.push(OrderPlaced);
             console.log("Next clicked!");
-        }
+            let newDate = this.selectedDate.day.getFullYear() + ' ' + 
+                                 Number(this.selectedDate.day.getMonth() + 1 )+ ' ' + 
+                                 this.selectedDate.day.getDate() + ' ' +
+                                 this.selectedDate.hour + ':' +
+                                 this.selectedDate.minute + ' ' +
+                                 this.selectedDate.amPm
+            console.log(new Date(newDate));
+    }
 }
