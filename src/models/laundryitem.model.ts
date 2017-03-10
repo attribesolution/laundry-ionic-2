2e144275de91961ec7c11fb7b12a17f9eeddc58d
Asad Khan <asad.simple92@gmail.com>
2017-03-10 22:03:38 +0500

    Notifications and Profile pages added

--- src/models/laundryitem.model.ts
+++ src/models/laundryitem.model.ts
@@ -7,6 +7,7 @@ interface LaundryItemData {
         wash: number,
         dryclean: number
     }
+
 }
 
 export interface LaundryItemModel{
--- src/pages/laundryitems/laundryitems.ts
+++ src/pages/laundryitems/laundryitems.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { ServicesPage } from '../services/services';
 import { LaundryItemsService } from './laundryitmes.service';
@@ -9,91 +9,112 @@ import {LaundryItemModel} from '../../models/laundryitem.model';
     providers:[LaundryItemsService] 
 })
 
-export class LaundryItems{
+export class LaundryItems implements OnInit{
   selectedItem: any;
   icons: string[];
   titles: string[];
-  items: Array<{title: string, icon: string, qty: number, price: number, total:number, dry:boolean, wash:boolean}>;
+  laundryitems : LaundryItemModel;
+  totalAmount : number;
+  totalQuantity : number;
+  responseArray : Array<Object> = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams,private items_Service: LaundryItemsService) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
+}
 
-    // Let's populate this page with some filler content for funzies
-   /* this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-     this.titles = ['Jacket', 'T-shirt', 'Jeans', 'Tides', 'Coat', 'Suit',
-    'Blanket', 'Socks', 'Curtains', 'Towels'];
-
-
-    this.items = [];
-    for (let i = 1; i < this.icons.length; i++) {
-      this.items.push({
-        title: this.titles[i],
-        icon: this.icons[i],
-        qty: 0,
-        price: i,
-        total: 0.0,
-        dry: false,
-        wash:false
-      });
-    }*/
-   let laundryitems : LaundryItemModel; 
-
-   var response$ =  items_Service.getItems()
-
+  ngOnInit(){
+     // default values
+    this.totalAmount   = 0;
+    this.totalQuantity = 0;
+    this.selectedItem = this.navParams.get('item');
+    var response$      =  this.items_Service.getItems()
     .subscribe(res => {
       if(res.status == 200) {
         let response = JSON.parse(res['_body']) 
-          laundryitems = {
+          this.laundryitems = {
             href : response["href"],
             data : response["data"]
           }
-          console.log('final laundry items: ', laundryitems)
+           this.maplaundryitems(this.laundryitems.data);
+          console.log('final laundry items: ', this.laundryitems)
         }
-        
-        
-        
     })
+    console.log("laundryitems",this.laundryitems);
+  }
 
-   
+maplaundryitems(data){
+
   
-   
-    console.log("laundryitems",laundryitems);
+  data.forEach(element => {
+    
+   let mappedObject =  {object_id: element.object_id, 
+    name: element.name,
+    icon: element.icon,
+    rate: element.rate,
+    toWash:true,
+	  toDry:false
 
+    }
+    this.responseArray.push(mappedObject);
+  });
 }
-
 // increment product qty
-  incrementQty(item) {
-    item.qty++;
-    item.total=item.price*item.qty;
-     console.log('Item '+ item.title+ 'Qty ' + item.qty + 'Price' + item.price + 'Total' + item.total);
+  incrementQty(item,index) {
+    this.totalQuantity++;
+
+     this.calculateTotalAmount(item);
+
+    console.log("totalQuantity = ",this.totalQuantity , "totalAmount = ",this.totalAmount);
+     
   }
 
   // decrement product qty
-  decrementQty(item) {
-    if(item.qty < 1 ){
-      item.qty = 0
-      item.total=item.price*item.qty;
-      console.log('Item '+ item.title+ 'Qty ' + item.qty + 'Price' + item.price + 'Total' + item.total);
+  decrementQty(item,index) {
+    if(this.totalQuantity < 1 ){
+      console.log("wash = " , item.rate.wash ,"totalQuantity = ",this.totalQuantity , "totalAmount = ",this.totalAmount);
+      this.totalQuantity = 0
+      this.totalAmount   = 0
+      
     }else{
-      item.qty--;
-      item.total=item.price*item.qty;
-       console.log('Item '+ item.title+ 'Qty ' + item.qty + 'Price' + item.price + 'Total' + item.total);
+      this.totalQuantity--;
+      
+      this.calculateTotalAmount(item);
    
     }
+
+    console.log("totalAmount after = ",this.totalAmount);
   }
 
+addWashingAmountToTotal(item) : number{
+
+   return item.rate.wash *this.totalQuantity  ;
+}
+addDryCleaningAmountToTotal(item) :number{
+
+  return item.rate.dryclean *this.totalQuantity  ;
+}
+calculateTotalAmount(item){
+
+ 
+  if(item.toWash && item.toDry)
+      this.totalAmount =   this.addWashingAmountToTotal(item) +  this.addDryCleaningAmountToTotal(item);
+  else if(item.toDry)
+       this.totalAmount =  this.addDryCleaningAmountToTotal(item);
+  else
+      this.totalAmount =   this.addWashingAmountToTotal(item) ;
+
+}
   wash(item)
   {
-    item.wash?item.wash=false:item.wash=true;
-    console.log('wash'+item.wash);
+     item.toWash = !item.toWash;
+     this.calculateTotalAmount(item);
+     console.log('wash'+item.toWash);
   }
   dry(item)
   {
-    item.dry?item.dry=false:item.dry=true;
-    console.log('dry'+item.dry);
+    
+     item.toDry = !item.toDry;
+     this.calculateTotalAmount(item);
+     console.log('dry'+item.toDry);
   }
 
  startNextScreen()
