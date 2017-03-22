7a2c9d1d11355448864e69a030ba76394053a020
Syed M. Taha <s.m.taha10@gmail.com>
2017-03-22 16:43:11 +0500

    Rate List Column Added

---
+++ src/pages/rates-list/rates-list.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { LaundryItemsService } from '../laundryitems/laundryitems.service';
+import { KeysPipe } from '../../pipes/ObjectPipe';
+@Component({
+    templateUrl: 'rates-list.html',
+    selector: 'rates-list',
+    providers: [LaundryItemsService, KeysPipe]
+})
+export class RatesListComponent{
+    laundryItems: any;
+    responseArray : Array<Object> = [];
+
+    constructor(private navCtrl: NavController, private laundryItemsService: LaundryItemsService){
+        laundryItemsService.getItems().subscribe(res=> {
+            if(res.status == 200) {
+                let response = JSON.parse(res['_body'])
+                this.laundryItems = {
+                    href: response["href"],
+                    data: response["data"]
+                }
+                this.maplaundryitems(this.laundryItems.data);
+                console.log(this.responseArray);
+                
+            }
+        });
+    }
+    maplaundryitems(data){
+        data.forEach(element => {
+            
+        let mappedObject =  {
+            object_id: element.object_id, 
+            name: element.name,
+            icon: element.icon,
+            rate: element.rate,
+            count: 0,
+            amount:0,
+            toWash:true,
+            toDry:false
+            }
+            this.responseArray.push(mappedObject);
+        });
+    }
+}
