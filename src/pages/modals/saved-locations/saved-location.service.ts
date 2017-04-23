ca0be10156b76af1cc518cda5622c8f2df1b9b6d
SMTaha <s.m.taha10@gmail.com>
2017-04-23 19:58:57 +0500

    Saved location fetched from server

--- src/pages/modals/saved-locations/saved-location.service.ts
+++ src/pages/modals/saved-locations/saved-location.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, RequestOptions, Headers } from '@angular/http';
+
 @Injectable()
 export class SavedLocationService{
     constructor(private http: Http){
     }
 
-    getAddressOfSavedLocations(URL, options?){
+    getAddressOfSavedLocations(URL, token){
+        let headers = new Headers({'x-access-token': token});
+        let options = new RequestOptions({ headers: headers });
         return this.http.get(URL, options);
     }
 }
