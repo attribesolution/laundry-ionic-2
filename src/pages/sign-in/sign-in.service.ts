6a91e86be1cd5ac02b25ba518f0083512186ab4b
SMTaha <s.m.taha10@gmail.com>
2017-04-24 00:11:16 +0500

    Facebook signin integrated

--- src/pages/sign-in/sign-in.service.ts
+++ src/pages/sign-in/sign-in.service.ts
@@ -5,6 +5,8 @@ export class SignInService{
     constructor(private http: Http){}
 
     signInUser = (URL, body): any => {
+        console.log(URL, body);
+        
         return this.http.post(URL, body)
     }
 }
